f6625d2f0c18b18875fadb86e0726404549451d5
Tiago Silva <tiago@guava.com.br>
2016-11-30 15:18:50 -0300

    Improves hash_rocket regex.

--- lib/upgrade_hash_rocket_syntax.rb
+++ lib/upgrade_hash_rocket_syntax.rb
@@ -18,7 +18,7 @@ class UpgradeHashRocketSyntax
 	private
 
 	HASH_ROCKET_REGEX = {
-		pattern: /:([a-z0-9_]+)\s=>(\s[a-zA-Z0-9_.'"{}]+)/, 
+		pattern: /:([a-z0-9_]+)\s=>(\s[a-zA-Z0-9_.'"{}\[\]]+)/, 
 		replacement: '\1:\2'
 	}
 
