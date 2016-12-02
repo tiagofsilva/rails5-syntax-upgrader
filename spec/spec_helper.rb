057d8b38c577c946fa65170efa66310974295726
Tiago Silva <tiago@guava.com.br>
2016-12-02 17:15:49 -0300

    Improves spec_helper config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,25 +1,8 @@
-require 'simplecov'
-SimpleCov.start
-
 require 'pry'
-require_relative './lib'
-
-Dir[
-  # Requires supporting ruby files with custom matchers and macros, etc,
-  # in spec/support/ and its subdirectories.
-  Rails.root.join('spec/support/**/*.rb'),
-  # Requires shared examples in spec/shared/ and its subdirectories.
-  Rails.root.join('spec/shared/**/*.rb')
-].each { |f| require f }
-
+require_relative '../lib/ruby_syntax_upgrader'
 
 RSpec.configure do |config|
-
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
   config.order = 'random'
-
+  config.color = true
 end
 
