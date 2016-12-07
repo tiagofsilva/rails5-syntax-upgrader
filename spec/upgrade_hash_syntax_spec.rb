a3552041a2a4c40032af4ac16cb90421b86153a7
tiagofsilva <aeroboom@gmail.com>
2016-12-07 03:45:46 -0300

    Fixes and refactors specs to use dynamic test files instead of static.

--- spec/upgrade_hash_syntax_spec.rb
+++ spec/upgrade_hash_syntax_spec.rb
@@ -2,7 +2,50 @@ require_relative 'spec_helper'
 
 describe RubySyntaxUpgrader::UpgradeHashRocketSyntax do
 
-  let(:hash_rocket_source_path) { 'spec/examples/hash_rocket_examples.txt' }
+  let!(:colon_hash_example) do
+    """
+    a = { bar: foo }
+    b = { bar: [1,2,3] }
+    c = { bar: 'foo', baz: 'foo' }
+    d = {bar: 10 }
+    e = { a: { b: { c: {} } } }
+    f = { bar: {foo: :foobar}}
+    g = bar: /[a-z0-9]*/
+    h = bar: /(.+)[:a-z]/
+    i = { bar: { foo: :baz } }
+    j = { bar: 1, foo: 2, baz: 3 }
+    """
+  end
+
+  let!(:hash_rocket_example) do
+    """
+    a = { :bar => foo }
+    b = { :bar => [1,2,3] }
+    c = { :bar=> 'foo', :baz => 'foo' }
+    d = {:bar=> 10 }
+    e = { :a=> { :b=> { :c=> {} } } }
+    f = { :bar=> {:foo=>:foobar}}
+    g = :bar => /[a-z0-9]*/
+    h = :bar => /(.+)[:a-z]/
+    i = { :bar => { foo: :baz } }
+    j = { :bar => 1, foo: 2, baz: 3 }
+    """
+  end
+
+  let(:hash_rocket_example_path) { './spec/temp' }
+  let(:hash_rocket_example_file) { "#{hash_rocket_example_path}/example_file.rb" }
+
+  before do
+    @source = File.open(hash_rocket_example_file, 'w') do |f|
+                f.write(hash_rocket_example)
+              end
+  end
+
+  after do
+    File.open(hash_rocket_example_file, 'w') do |f|
+      f.write('')
+    end
+  end
 
 	describe '#new' do
 		context 'when no argument is passed' do
@@ -11,28 +54,22 @@ describe RubySyntaxUpgrader::UpgradeHashRocketSyntax do
           .to raise_error(ArgumentError)
       end
     end
-
-    context 'when source is empty' do
-      it 'raises a custom error' do
-        expect{ RubySyntaxUpgrader::UpgradeHashRocketSyntax.new(source: nil) }
-          .to raise_error('Source file must be passed')
-      end
-    end
 	end
 
   describe '#execute' do
+    subject { RubySyntaxUpgrader::UpgradeHashRocketSyntax
+              .new(hash_rocket_example_path) 
+            }
 
-    context 'when commit is false' do
-      subject { RubySyntaxUpgrader::UpgradeHashRocketSyntax
-                .new(source: hash_rocket_source_path, commit: false) 
-              }
+    it 'replaces file content' do
+      subject.execute
+      content = File.read(hash_rocket_example_file)
+      expect(content).to eq colon_hash_example
+    end
 
-      let!(:expected_content) { File.read('spec/examples/colon_hash_examples.txt') }
-
-      it 'replaces content without commiting' do
-        result_content = subject.execute
-        expect(result_content).to eq expected_content
-      end
+    it 'returns all selected for the given source path' do
+      files = subject.execute
+      expect(files).to eq [hash_rocket_example_file]
     end
 
   end
