bb149ab1bf0d325bfb381d409a5dbfdd08e558ec
Tiago Silva <tiago@guava.com.br>
2016-12-09 19:43:58 -0300

    Starts implementation of tests for controller_test_syntax

---
+++ lib/ruby_syntax_upgrader/upgrade_rails_controller_test_syntax.rb
@@ -0,0 +1,31 @@
+module RubySyntaxUpgrader
+  class UpgradeRailsControllerTestSyntax
+    include RubySyntaxUpgrader::RegexReplacer
+
+    def initialize(source)
+      @source = source
+      raise 'Source file must be passed' unless source
+    end
+
+    def execute
+      regex_replace(
+        source: source,
+        pattern: TEST_CASE_CONTROLLER_REQUEST_REGEX[:pattern],
+        replacement: TEST_CASE_CONTROLLER_REQUEST_REGEX[:replacement]
+      )
+    end
+
+    private
+
+    RUBY_SYMBOL_REGEX = /:[a-z0-9_]+/
+    RUBY_STRING_REGEX = /[a-z0-9_'"]+/
+    CONTROLLER_ACTION_REGEX = /#{RUBY_SYMBOL_REGEX}|RUBY_STRING_REGEX/
+    HTTP_METHODS_REGEX = /get|post|patch|put|delete/
+
+    TEST_CASE_CONTROLLER_REQUEST_REGEX = {
+        pattern: /((#{HTTP_METHODS_REGEX})\s(#{CONTROLLER_ACTION_REGEX})\,)((\s+[a-z0-9_]+:\s.+\,?)+)/,
+        replacement: '\1 params: {\4 }'
+    }
+    
+  end
+end
