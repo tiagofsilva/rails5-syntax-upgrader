090d0e600e9aa8c3807e5631dec66a1d0e0331ab
tiagofsilva <aeroboom@gmail.com>
2017-01-11 03:53:26 -0300

    Adds tests to controller test syntax upgrader (hash rocket style terms)

--- lib/ruby_syntax_upgrader/upgrade_controller_test_syntax.rb
+++ lib/ruby_syntax_upgrader/upgrade_controller_test_syntax.rb
@@ -16,11 +16,13 @@ module RubySyntaxUpgrader
 
     RUBY_SYMBOL_REGEX = /:[a-z0-9_]+/
     RUBY_STRING_REGEX = /[a-z0-9_'"]+/
+    HASH_ASSERTION = /([a-z0-9_]+:|#{RUBY_SYMBOL_REGEX}\s*\=\>)\s*.+/
+    NOT_PARAMS_HASH = /^(?!params:)/
     CONTROLLER_ACTION_REGEX = /#{RUBY_SYMBOL_REGEX}|#{RUBY_STRING_REGEX}/
     HTTP_METHODS_REGEX = /get|post|patch|put|delete/
 
     TEST_CASE_CONTROLLER_REQUEST_REGEX = {
-        pattern: /((#{HTTP_METHODS_REGEX})\s(#{CONTROLLER_ACTION_REGEX})\,)((\s+[a-z0-9_]+:\s+.+\,?)+)/,
+        pattern: /((#{HTTP_METHODS_REGEX})\s(#{CONTROLLER_ACTION_REGEX})\,)((\s+#{HASH_ASSERTION}\,?)+)/,
         replacement: '\1 params: {\4 }'
     }
     
--- spec/upgrade_controller_test_syntax_spec.rb
+++ spec/upgrade_controller_test_syntax_spec.rb
@@ -53,45 +53,40 @@ describe RubySyntaxUpgrader::UpgradeControllerTestSyntax do
     end
 
     context 'with params using hash rocket style' do
-      
+      it 'replaces with numeric value' do
+        expect(subject.replace("get :index, :store => 1")).to eq "get :index, params: { :store => 1 }"
+      end
+
+      it 'replaces with string value' do
+        expect(subject.replace("get :index, :store => '1'")).to eq "get :index, params: { :store => '1' }"
+      end
+
+      it 'replaces with a variable(method call) value' do
+        expect(subject.replace("get :index, :store=> store.id")).to eq "get :index, params: { :store=> store.id }"
+      end
+
+      it 'replaces with multiple variables values' do
+        expect(subject.replace("get :index, :store=> store.id, :box_type=> box_type.id")).to eq "get :index, params: { :store=> store.id, :box_type=> box_type.id }"
+      end
+
+      it 'replaces with multiple variables with different type of values' do
+        expect(subject.replace("get :index, :store => store.id, :sort => 'asc'")).to eq "get :index, params: { :store => store.id, :sort => 'asc' }"
+      end
+
+      it 'replaces with any http method and any digit number' do
+        expect(subject.replace("get :put, :id => 9999")).to eq "get :put, params: { :id => 9999 }"
+      end
+
+      xit 'does not replace when param already exist' do
+        expect(subject.replace("put :update, params: { :id => 9999 }")).to eq "put :update, params: { :id => 9999 }"
+      end
+
+      it 'replaces when call has multiple lines' do
+        expect(subject.replace("""put :update, :id=> reservation.id, :reservation=> \
+        { box_type_id: store.box_type.id }""")).to eq """put :update, params: { :id=> reservation.id, :reservation=> \
+        { box_type_id: store.box_type.id } }"""
+      end
+
     end
-
   end
-
-  let!(:old_controller_example) do
-    """
-      get :index
-      get :index, :store => 1
-      post :index, :store => '1'
-      patch :index, :store => store.id
-      put :index, :store => store.id
-      delete :index, :store => store.id
-      get :index, :store => store.id, :box_type => box_type.id
-      get :index, store: store.id, box_type: box_type.id
-      get :index, :store => store.id, :sort => 'box_type', :direction => 'desc'
-      put :update, id: 9999
-      put :update, params: { id: 9999 }
-      put :update, :id => reservation.id, :reservation => 
-        { :box_type_id => store.box_type.id }
-    """
-  end
-
-  let!(:new_controller_example) do
-    """
-      get :index
-      get :index, params: { :store => 1 }
-      post :index, params: { :store => '1' }
-      patch :index, params: { :store => store.id }
-      put :index, params: { :store => store.id }
-      delete :index, params: { :store => store.id }
-      get :index, params: { :store => store.id, :box_type => box_type.id }
-      get :index, params: { store: store.id, box_type: box_type.id }
-      get :index, params: { :store => store.id, :sort => 'box_type', :direction => 'desc' }
-      put :update, id: 9999
-      put :update, params: { id: 9999 }
-      put :update, params: { :id => reservation.id, :reservation => 
-        { :box_type_id => store.box_type.id } }
-    """
-  end
-
 end
