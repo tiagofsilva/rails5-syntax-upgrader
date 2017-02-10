97b9134bba39c330ad027375a562e65257ef924d
tiagofsilva <aeroboom@gmail.com>
2017-02-10 02:02:59 -0300

    Uses Helper module.

--- lib/ruby_syntax_upgrader.rb
+++ lib/ruby_syntax_upgrader.rb
@@ -7,19 +7,21 @@ module RubySyntaxUpgrader
 	  namespace :syntax do
 	    desc "Upgrades Ruby's old hash rocket syntax to column syntax"
 	    task :hash, [:source] do |t, args|
-	      upgrade_syntax(args.source, t.name)
+	      Helper.upgrade_syntax(args.source, t.name)
 	    end
 
 	    desc "Upgrades Rails controller tests to use Rails 5 syntax"
 	    task :controller_test_case, [:source] do |t, args|
-	      upgrade_syntax(args.source, t.name)
-	    end
-
-	    def upgrade_syntax(source, task)
-	      type = task.split(':').last
-	      RubySyntaxUpgrader::UpgradeSyntax.new(source, type.to_sym).execute
+	      Helper.upgrade_syntax(args.source, t.name)
 	    end
 	  end  
+	end
+
+	module Helper
+    def self.upgrade_syntax(source, task)
+      type = task.split(':').last
+      RubySyntaxUpgrader::UpgradeSyntax.new(source, type.to_sym).execute
+    end
 	end
 end
 
