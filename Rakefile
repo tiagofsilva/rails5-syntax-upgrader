130f090f44305448c11005f82dbdb580a3522006
tiagofsilva <aeroboom@gmail.com>
2017-02-10 01:55:53 -0300

    Exposing rake tasks to be used in other projects

--- Rakefile
+++ Rakefile
@@ -1,7 +1,6 @@
 require 'bundler/gem_tasks'
 
 require_relative 'lib/ruby_syntax_upgrader'
-load 'lib/tasks/upgrade_syntax_tasks.rake'
 
 # RSpec
 require 'rspec/core/rake_task'
--- lib/ruby_syntax_upgrader.rb
+++ lib/ruby_syntax_upgrader.rb
@@ -1,6 +1,28 @@
 require 'rake'
 
 module RubySyntaxUpgrader
+	extend Rake::DSL
+
+	namespace :upgrade do
+	  namespace :syntax do
+	    desc "Upgrades Ruby's old hash rocket syntax to column syntax"
+	    task :hash, [:source] do |t, args|
+	      Helper.upgrade_syntax(args.source, t.name)
+	    end
+
+	    desc "Upgrades Rails controller tests to use Rails 5 syntax"
+	    task :controller_test_case, [:source] do |t, args|
+	      Helper.upgrade_syntax(args.source, t.name)
+	    end
+	  end  
+	end
+
+	module Helper
+    def self.upgrade_syntax(source, task)
+      type = task.split(':').last
+      RubySyntaxUpgrader::UpgradeSyntax.new(source, type.to_sym).execute
+    end
+	end
 end
 
 require_relative 'ruby_syntax_upgrader/regex_replacer'
