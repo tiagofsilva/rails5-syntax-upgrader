edba697f3ba514cbdbb2436702b747355540826a
tiagofsilva <aeroboom@gmail.com>
2017-02-10 01:55:53 -0300

    Testing internalizing rake task.

--- lib/ruby_syntax_upgrader.rb
+++ lib/ruby_syntax_upgrader.rb
@@ -1,6 +1,26 @@
 require 'rake'
 
 module RubySyntaxUpgrader
+	extend Rake::DSL
+
+	namespace :upgrade do
+	  namespace :syntax do
+	    desc "Upgrades Ruby's old hash rocket syntax to column syntax"
+	    task :hash, [:source] do |t, args|
+	      upgrade_syntax(args.source, t.name)
+	    end
+
+	    desc "Upgrades Rails controller tests to use Rails 5 syntax"
+	    task :controller_test_case, [:source] do |t, args|
+	      upgrade_syntax(args.source, t.name)
+	    end
+
+	    def upgrade_syntax(source, task)
+	      type = task.split(':').last
+	      RubySyntaxUpgrader::UpgradeSyntax.new(source, type.to_sym).execute
+	    end
+	  end  
+	end
 end
 
 require_relative 'ruby_syntax_upgrader/regex_replacer'
